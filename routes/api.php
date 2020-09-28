812c63e10c23378af2689f5804e962bdde362704
yuanshuxin <307214073@qq.com>
2020-09-28 08:46:32 +0800

    袁树鑫的第一次提交

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('stuadmin')->namespace('StuAdmin')->group(function (){
+
+    Route::get('/insert','StuLeaderController@insert');
+    Route::get('/getall','StuLeaderController@getAll');
+    Route::get('/getbyid','StuLeaderController@getById');
+    Route::get('/getupdateinfo','StuLeaderController@getUpdateInfo');
+    Route::get('/update','StuLeaderController@update');
+    Route::get('/index','StuLeaderController@index');
+
+});
+
